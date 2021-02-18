d85cb4c42141d0d9f1faabee34e1973e02b98235
= <=>
2021-02-18 22:51:25 +0700

    Simp Pertama

---
+++ src/config/index.js
@@ -0,0 +1,28 @@
+export default {
+  ucapan: [
+    "Hai kamu, Apa kabar?",
+    "Kuharap kamu baik-baik aja ya disana",
+    "meskipun aku tau kamu lagi pusing",
+    "memikirkan segala hal yang saat ini kamu hadapi",
+    "aku yakin, kamu pasti bisa melewatinya",
+    "aku selalu support dari kejauhan meskipun kita tak sedekat dulu lagi",
+    "kalau kamu lagi down, sedih, nangis, seneng",
+    "aku selalu siap 24 jam untuk mendengarkan keluh kesahmu apapun itu",
+    "jadi tetap semangat ya!",
+    "I wuv chuuuu! UwU",
+  ],
+  panggilan: ["Siti Annisa Azzara ❣️"],
+  colors: [
+    "#FF6900",
+    "#FCB900",
+    "#7BDCB5",
+    "#00D084",
+    "#8ED1FC",
+    "#0693E3",
+    "#ABB8C3",
+    "#EB144C",
+    "#F78DA7",
+    "#CC00FF",
+    "#000000",
+  ],
+};
